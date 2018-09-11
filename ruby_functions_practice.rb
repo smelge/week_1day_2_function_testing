355a296797a3884f337e7c9ab776ac6f366b6c83
jordanraitt <jordanraitt@hotmail.com>
2018-09-11 15:28:32 +0100

    number to real months added

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,34 @@ end
 def add_string_as_number(string_1, string_2)
 return string_1.to_i + string_2.to_i
 end
+
+def number_to_full_name__month_1(monthIn)
+  case monthIn
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "Junevember"
+  when 8
+    return "August"
+  when 9
+    return "Speptember"
+  when 10
+    return "October"
+  when 11
+    return "Depression Month"
+  when 12
+    return "Expenses"
+  else
+    return "Return to planet please"
+  end
+end
